962bea7bf49109f4da4e61f61ae4e7307e92d318
Kutalov Konstantin <kutalov.k.v@mail.ru>
2023-11-18 04:28:33 +0700

    Name           Stmts   Miss  Cover ---------------------------------- __init__.py        0      0   100% test_item.py      23      1    96% ---------------------------------- TOTAL             23      1    96%

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,17 +1,31 @@
+# -*- coding: utf-8 -*-
 import unittest
 from src.item import Item
 
 
-class TestItemMethods(unittest.TestCase):
-    def setUp(self):
-        self.item = Item("TestItem", 10.0, 3)
+class TestItem(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        Item.instantiate_from_csv()
 
-    def test_calculate_total_price(self):
-        self.assertEqual(self.item.calculate_total_price(), 30.0)
+    def test_set_name(self):
+        item = Item('Smartphone', 1000, 5)
+        self.assertEqual(item.name, 'Smartphone')
 
-    def test_apply_discount(self):
-        self.item.apply_discount(0.9)
-        self.assertEqual(self.item.pay_rate, 0.9)
+        item.name = 'SuperSmart'
+        self.assertEqual(item.name, 'SuperSmart')
+
+    def test_instantiate_from_csv(self):
+        Item.instantiate_from_csv()
+        self.assertEqual(len(Item.all), 10)
+        self.assertIsInstance(Item.all[0], Item)
+        self.assertAlmostEqual(Item.all[0].price, 100)
+        self.assertEqual(Item.all[0].quantity, 1)
+
+    def test_string_to_number(self):
+        number_string = '10.5'
+        number = Item.string_to_number(number_string)
+        self.assertEqual(number, 10.5)
 
 
 if __name__ == '__main__':
